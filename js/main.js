8d03955cebe4ef839c25b80b81e3f4771a2bdf93
saifeddine ghouma <saifeddinegouma@yahoo.fr>
2019-11-28 20:56:50 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+jQuery(document).ready(function(){
+
+    "use strict";
+  
+    $('#slider-carousel').carouFredSel({
+      responsive:true,
+      width:'100%',
+      circular:true,
+      scroll:{
+        items:1,
+        duration:500,
+        pauseOnHover: true
+      },
+      auto:true,
+      items:{
+        visible:{
+          min:1,
+          max:1
+        },
+        height:"auto"
+       },
+       pagination  : {
+        container       : ".sliderpager",
+        anchorBuilder   : false
+    }
+    });
+    $(window).scroll(function(){
+        var top = $(window).scrollTop();
+        if(top>=112)
+        {
+            $("header").addClass('secondary-dark-blue-bg');
+        }
+        else
+            if($("header").hasClass('secondary-dark-blue-bg')){
+                $("header").removeClass('secondary-dark-blue-bg')
+            }
+    })
+  
+  });
