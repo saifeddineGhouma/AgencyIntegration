e7bd7180433712b90a1287b3994226269a302710
saifeddine ghouma <saifeddinegouma@yahoo.fr>
2019-12-22 14:04:12 +0100

    all template not respansive

--- js/main.js
+++ js/main.js
@@ -24,6 +24,77 @@ jQuery(document).ready(function(){
         anchorBuilder   : false
     }
     });
+
+
+    $('.portfolio-carousel').carouFredSel({
+        responsive:true,
+        width:'100%',
+        circular:true,
+        prev:'#prev',
+        next:'#next',
+        scroll:{
+          items:1,
+          duration:500,
+          pauseOnHover: true
+        },
+        auto:true,
+        items:{
+          visible:{
+            min:1,
+            max:4
+          },
+          height:"auto"
+         }
+      });
+
+
+
+      $('.team-carousel').carouFredSel({
+        responsive:true,
+        width:'100%',
+        circular:true,
+        prev:'#team_prev',
+        next:'#team_next',
+        scroll:{
+          items:1,
+          duration:500,
+          pauseOnHover: true
+        },
+        auto:true,
+        items:{
+          visible:{
+            min:1,
+            max:4
+          },
+          height:"auto"
+         }
+      });
+
+
+
+      $('.testimonials-carousel').carouFredSel({
+        responsive:true,
+        width:'100%',
+        circular:true,
+        scroll:{
+          items:1,
+          duration:500,
+          pauseOnHover: true
+        },
+        auto:true,
+        items:{
+          visible:{
+            min:1,
+            max:1
+          },
+          height:"variable"
+         },
+         pagination  : {
+          container       : ".testipage",
+          anchorBuilder   : false
+      }
+      });
+
     $(window).scroll(function(){
         var top = $(window).scrollTop();
         if(top>=112)
